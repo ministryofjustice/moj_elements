fb901ce526b702f9738ef1fb9eb55d103d0e71b1
Mat Harden <matharden@gmail.com>
2014-11-04 15:12:22 +0000

    Add tabs module

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,33 +6,45 @@ module.exports = function(grunt) {
     // Task configuration.
     jshint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
+        jshintrc: '.jshintrc'
       },
       gruntfile: {
         src: 'Gruntfile.js'
       },
-      // lib_test: {
-      //   src: ['lib/**/*.js', 'test/**/*.js']
-      // }
+      javascripts: {
+        src: ['src/javascripts/**/*.js']
+      }
+    },
+    sass: {
+      build: {
+        options: {
+          sourcemap: 'none'
+        },
+        files: {
+          'build/stylesheets/moj.tabs.css': 'src/stylesheets/moj.tabs.scss'
+        }
+      }
     },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
+      },
+      javascripts: {
+        files: '<%= jshint.javascripts.src %>',
+        tasks: ['jshint:javascript']
+      },
+      stylesheets: {
+        files: '<%= jshint.stylesheets.src %>',
+        tasks: ['sass:stylesheets']
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/',
+        src: 'javascripts/**',
+        dest: 'build/'
       }
     }
   });
@@ -40,8 +52,10 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'sass', 'copy']);
 
 };
